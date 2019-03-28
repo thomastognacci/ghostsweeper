4b06167fa83cecf1178a6fb6835d93a636555693
thomastognacci <thomas.tognacci@gmail.com>
2019-03-28 20:55:21 -0230

    Updates

---
+++ precache-manifest.6ced9e6e61acaec7a599d3a964a73294.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c85a59fdaec6e7210126",
+    "url": "/ghostsweeper/static/js/runtime~main.c85a59fd.js"
+  },
+  {
+    "revision": "1ba4a5bd2f3f6f9e9e9d",
+    "url": "/ghostsweeper/static/js/main.1ba4a5bd.chunk.js"
+  },
+  {
+    "revision": "488285705931d0fcca17",
+    "url": "/ghostsweeper/static/js/2.48828570.chunk.js"
+  },
+  {
+    "revision": "1ba4a5bd2f3f6f9e9e9d",
+    "url": "/ghostsweeper/static/css/main.6952ca3e.chunk.css"
+  },
+  {
+    "revision": "17d382f5e2cfe2cbbd74d546326bb219",
+    "url": "/ghostsweeper/index.html"
+  }
+];
